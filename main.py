102be570bda9114d601bf9dbdab99486c9bff563
Ash Srinivas <ash@supallama.ai>
2024-09-26 00:57:59 -0700

    Added additional type annotation to CLI arguments

--- main.py
+++ main.py
@@ -1,19 +1,64 @@
+import os
+from typing_extensions import Annotated
+
+from dotenv import load_dotenv
+
+from fireworks.client import Fireworks
+
 import typer
 
+
+# Load environment variables``
+load_dotenv()
+
+# Create CLI app
 app = typer.Typer()
 
-
-@app.command()
-def hello(name: str):
-    print(f"Hello {name}")
+# Create Fireworks client
+client = Fireworks(api_key=os.getenv("FIREWORKS_AI_KEY")) 
 
 
 @app.command()
-def goodbye(name: str, formal: bool = False):
-    if formal:
-        print(f"Goodbye Mr. {name}. Have a good day.")
-    else:
-        print(f"Bye {name}!")
+def chat_with_llama(content: Annotated[str, typer.Argument()] = "Tell me a family-friendly joke"):
+    """
+    Chat with 🦙 Llama 3.1 8B Instruct (serverless)
+    """
+    response = client.chat.completions.create(
+        model="accounts/fireworks/models/llama-v3p1-8b-instruct",
+        messages=[{
+            "role": "user",
+            "content": content, 
+        }],
+    )
+    print(response.choices[0].message.content)
+
+
+@app.command()
+def chat_with_mixtral(content: Annotated[str, typer.Argument()] = "Tell me a family-friendly joke"):
+    """
+    Chat with Ⓜ️  Mixtral 8x7B Instruct 
+    """
+    response = client.chat.completions.create(
+        model="accounts/fireworks/models/mixtral-8x7b-instruct",
+        messages=[{
+            "role": "user",
+            "content": content, 
+        }],
+    )
+    print(response.choices[0].message.content)
+
+
+@app.command()
+def fine_tune(
+    model: Annotated[str, typer.Argument()], 
+    with_data: str = None,
+    with_webpage: str = None,
+    ):
+    """
+    🛠️📉📈 Fine-tune a 🤖 Model 🦾 with 💽 data in a 💾 file or a 🌐 webpage 📄
+    """
+    if with_webpage:
+        print(f"Okay, which page?")
 
 
 if __name__ == "__main__":
